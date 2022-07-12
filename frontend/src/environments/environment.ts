276fa0ba5adaa9cff2f607281fe18993be97e81b
Samuel George <georgesam.iv@gmail.com>
2022-07-12 11:32:51 +0100

    Hide secrets

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,14 +1,15 @@
-/* @TODO replace with your variables
+/* @TODO replace with your variablesA
  * ensure all variables on this page match your project
  */
+import {API_SERVER_URL, AUTH0_CLIENT_ID, API_AUTH0_AUDIENCE, AUTH0_DOMAIN, AUTH0_CALLBACK_URL} from '../../secret.js';
 
 export const environment = {
   production: false,
-  apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
+  apiServerUrl: API_SERVER_URL, // the running FLASK api server url
   auth0: {
-    url: 'unnamed-devs.us', // the auth0 domain prefix
-    audience: 'Drink', // the audience set for the auth0 app
-    clientId: 'QPkH1Ynnk2SGydMOE86dXZneGpWteAjR', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8103', // the base url of the running ionic application. 
+    url: AUTH0_DOMAIN, // the auth0 domain prefix
+    audience: API_AUTH0_AUDIENCE, // the audience set for the auth0 app
+    clientId: AUTH0_CLIENT_ID, // the client id generated for the auth0 app
+    callbackURL: AUTH0_CALLBACK_URL, // the base url of the running ionic application. 
   }
 };
